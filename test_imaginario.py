b5e42f9ab24c1e739194f248880c4f224e963c4d
moisesGo <moy.gonzalez@gmail.com>
2024-02-14 10:56:13 +0100

    fix: fix tests fixture usage

--- test_imaginario.py
+++ test_imaginario.py
@@ -14,17 +14,17 @@ def test_numbers():
     return a, b
 
 
-def test_addition(test_values):
+def test_addition(test_numbers):
     """Test addition operation"""
-    a, b = test_values
+    a, b = test_numbers
     result_addition = a + b
     assert result_addition.real == 3.0
     assert result_addition.imag == 5.0
 
 
-def test_subtraction(test_values):
+def test_subtraction(test_numbers):
     """Test subtraction operatio"""
-    a, b = test_values
+    a, b = test_numbers
     result_subtraction = a - b
     assert result_subtraction.real == -1.0
     assert result_subtraction.imag == -3.0
